cfda67de06ee90d06a1f44ceff5a3335f8e9e870
Kavya <kavyagopal94@gmail.com>
2024-06-27 16:01:25 +0530

    No_Of_Roles_Counted

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,14 +5,16 @@ import java.util.Random;
 public class SnakeAndLadder {
 
     public static void main(String[] args) {
-        int position = 0;
+        int position = 0, c = 0;
         Random random = new Random();
 
         while (position < 100) {
-            // Roll the die to get a number between 1 and 6
-            int diceRoll = random.nextInt(6) + 1;
+            
+            int diceRoll = random.nextInt(7);
             System.out.println("Rolled: " + diceRoll);
-
+            c++;
+            System.out.println("Number of Times Rolled: " + c);
+            
             int option = random.nextInt(3);
             switch (option) {
                 case 0:
@@ -23,12 +25,18 @@ public class SnakeAndLadder {
                 case 1:
                     // Ladder
                     position += diceRoll;
+                    if(position > 100)
+                    {
+                    	position -= position;
+                    }
                     System.out.println("Ladder! Move ahead by " + diceRoll + " positions.");
                     break;
+                    
                 case 2:
                     // Snake
                     position -= diceRoll;
-                    if (position < 0) {
+                    if (position < 0) 
+                    {
                         position = 0;
                     }
                     System.out.println("Snake! Move behind by " + diceRoll + " positions.");
